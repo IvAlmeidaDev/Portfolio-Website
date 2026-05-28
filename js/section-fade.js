960c73edc1f75e0abcbdcbdea81e681128712aef
Ivan Almeida <ivan.almeida28@yahoo.com>
2026-05-28 11:23:28 -0500

    Added side section fading

---
+++ js/section-fade.js
@@ -0,0 +1,17 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+        else{
+            entry.target.classList.remove("show");
+        }
+    });
+},{
+    threshold: 0.12,
+    rootMargin: "-15% 0px -15% 0px"
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
