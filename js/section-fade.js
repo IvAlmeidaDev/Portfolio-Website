ae35236bb6fd8b4083ffc712550783fbe0bd0863
Ivan Almeida <ivan.almeida28@yahoo.com>
2026-05-28 19:18:38 -0500

    Modified fade-in animation for the project cards so that they all animate individually

--- js/section-fade.js
+++ js/section-fade.js
@@ -1,17 +1,50 @@
 const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenTitles = document.querySelectorAll(".hidden_title");
+const hiddenProjects = document.querySelectorAll(".project_hidden");
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-        else{
-            entry.target.classList.remove("show");
-        }
-    });
-},{
-    threshold: 0.12,
-    rootMargin: "-15% 0px -15% 0px"
+  entries.forEach((entry) => {
+
+    // SECCIONES
+    if (entry.target.classList.contains("hidden")) {
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+
+    }
+
+    // PROYECTOS
+    if (entry.target.classList.contains("project_hidden")) {
+
+      if (entry.isIntersecting) {
+        entry.target.classList.add("project_show");
+      } else {
+        entry.target.classList.remove("project_show");
+      }
+
+    }
+
+    // TÍTULOS
+if (entry.target.classList.contains("hidden_title")) {
+
+  if (entry.isIntersecting) {
+    entry.target.classList.add("show_title");
+  } else {
+    entry.target.classList.remove("show_title");
+  }
+
+}
+
+  });
+},
+{
+  threshold: 0.15,
+  rootMargin: "-10% 0px -10% 0px"
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+hiddenProjects.forEach((el) => observer.observe(el));
+hiddenTitles.forEach((el) => observer.observe(el));
